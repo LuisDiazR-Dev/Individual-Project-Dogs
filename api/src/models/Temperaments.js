442162ff7be7d4a9c1815cd2819549ebf8c24d7c
LuisDiazR-Dev <diazr.info@gmail.com>
2024-04-26 19:36:49 -0500

    modelos creados

--- api/src/models/Temperaments.js
+++ api/src/models/Temperaments.js
@@ -3,30 +3,15 @@ const { DataTypes } = require('sequelize');
 // Luego le inyectamos la conexión a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('temperaments', {
     id: {
       primaryKey: true, 
       type: DataTypes.UUID, // Tipo UUID para un identificador único
       defaultValue: DataTypes.UUIDV4, // Valor por defecto que será un UUID generado automáticamente
     }, 
-    img: {
-      type: DataTypes.STRING, // STRING para la URL de la imagen
-    },
     name: {
       type: DataTypes.STRING, // Tipo STRING para el nombre del perro
       allowNull: false, // no puede ser nulo
-    },
-    height: {
-      type: DataTypes.FLOAT, // Tipo FLOAT para la altura del perro (puede ser un número decimal)
-    },
-    weight: {
-      type: DataTypes.FLOAT, // Tipo FLOAT para el peso del perro (puede ser un número decimal)
-    },
-    age: {
-      type: DataTypes.INTEGER, // Tipo INTEGER para la edad del perro (número entero)
-    },
-    isActive: {
-			    type: DataTypes.BOOLEAN,
-			},
+    }
   });
 };
